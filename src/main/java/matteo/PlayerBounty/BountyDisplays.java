0040f9fdc57d36697b9e79dc98aff2a1e645c9d3
matteo335 <79221584+matteo335@users.noreply.github.com>
2025-09-29 21:47:12 +0200

    1.21.1

--- src/main/java/matteo/PlayerBounty/BountyDisplays.java
+++ src/main/java/matteo/PlayerBounty/BountyDisplays.java
@@ -1,59 +1,30 @@
 package matteo.PlayerBounty;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.network.NetworkEvent.Context;
 
-import java.util.function.Supplier;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-public class BountyDisplays {
+public record BountyDisplays(String bountyDisplay1, int bounty, String bountyDisplay2, int entityID, boolean deleteDisplay) implements CustomPacketPayload {
 
-    public String bountyDisplay1;
-    public int bounty;
-    public String bountyDisplay2;
-
-    public int entityId;
-    public int deleteDisplay;
-
-    public BountyDisplays(FriendlyByteBuf buf) {
-        this.bountyDisplay1 = buf.readUtf();
-        this.bounty = Integer.parseInt(buf.readUtf());
-        this.bountyDisplay2 = buf.readUtf();
-        this.entityId = buf.readInt();
-        this.deleteDisplay = buf.readInt();
-    }
-
-    public BountyDisplays(String bountydisplay1, int bounty, String bountydisplay2, int entityID, int deleteDisplay) {
-        this.bountyDisplay1 = bountydisplay1;
-        this.bounty = bounty;
-        this.bountyDisplay2 = bountydisplay2;
-        this.entityId = entityID;
-        this.deleteDisplay = deleteDisplay;
-    }
-
-
-    public void toBytes(FriendlyByteBuf buf) {
-        buf.writeUtf(bountyDisplay1);
-        buf.writeUtf(String.valueOf(bounty));
-        buf.writeUtf(bountyDisplay2);
-        buf.writeInt(entityId);
-        buf.writeInt(deleteDisplay);
-    }
-
-    public void handle(Supplier<Context> ctx) {
-        ctx.get().enqueueWork(() -> {
+    public void payload(IPayloadContext ctx) {
+        ctx.enqueueWork(() -> {
             Minecraft minecraft = Minecraft.getInstance();
 
-            Player toSync = (Player) minecraft.level.getEntity(entityId);
+            Player toSync = (Player) minecraft.level.getEntity(entityID);
 
             if (toSync != null) {
-                ctx.get().setPacketHandled(true);
 
                 PlayerBounty.bountyTags(toSync, bountyDisplay1, bounty, bountyDisplay2, deleteDisplay);
                 minecraft.player.connection.getPlayerInfo(toSync.getGameProfile().getId()).setTabListDisplayName(Component.translatable(bountyDisplay1 + bounty + bountyDisplay2));
             }
         });
+    }
+
+    @Override
+    public Type<? extends CustomPacketPayload> type() {
+        return PlayerBounty.TYPE;
     }
 }
--- src/main/java/matteo/PlayerBounty/DisplayEvents.java
+++ src/main/java/matteo/PlayerBounty/DisplayEvents.java
@@ -4,34 +4,32 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.PacketDistributor;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
-import java.util.Objects;
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
+@EventBusSubscriber
 public class DisplayEvents {
-
-    @SubscribeEvent
-    public static void renderName(PlayerEvent.NameFormat event) {
-        CompoundTag tag = event.getEntity().getPersistentData();
-        if (tag.contains("bounty")) { event.setDisplayname(Component.translatable(tag.getString("bountydisplay1") + tag.getInt("bounty") + tag.getString("bountydisplay2"))); }
-    }
 
     @SubscribeEvent
     public static void onJoinWorld(PlayerEvent.PlayerLoggedInEvent event) {
         Player player = event.getEntity();
         if (!player.getCommandSenderWorld().isClientSide) {
             if (player.getPersistentData().contains("bounty")) {
-                PlayerBounty.sendPacket(player, player.getPersistentData().getString("bountydisplay1"), player.getPersistentData().getInt("bounty"), player.getPersistentData().getString("bountydisplay2"), 0);
+                PlayerBounty.packets((ServerPlayer) player, player.getPersistentData().getString("bountydisplay1"), player.getPersistentData().getInt("bounty"), player.getPersistentData().getString("bountydisplay2"), false);
 
-                for (Player playerlist : (Objects.requireNonNull(player.getServer())).getPlayerList().getPlayers()) {
-                    PlayerBounty.network.send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) player), new BountyDisplays(playerlist.getPersistentData().getString("bountydisplay1"), playerlist.getPersistentData().getInt("bounty"), playerlist.getPersistentData().getString("bountydisplay2"), playerlist.getId(), 0));
+                for (Player playerlist : player.getServer().getPlayerList().getPlayers()) {
+                    PacketDistributor.sendToPlayer((ServerPlayer) player, new BountyDisplays(playerlist.getPersistentData().getString("bountydisplay1"), playerlist.getPersistentData().getInt("bounty"), playerlist.getPersistentData().getString("bountydisplay2"), playerlist.getId(), false));
                 }
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void renderName(PlayerEvent.NameFormat event) {
+        CompoundTag tag = event.getEntity().getPersistentData();
+        if (tag.contains("bounty")) { event.setDisplayname(Component.translatable(tag.getString("bountydisplay1") + tag.getInt("bounty") + tag.getString("bountydisplay2"))); }
     }
 
     @SubscribeEvent
@@ -54,7 +52,7 @@ public class DisplayEvents {
         if (event.getTarget() instanceof Player player) {
             if (player.getPersistentData().contains("bounty")) {
                 ServerPlayer display = (ServerPlayer) event.getEntity();
-                PlayerBounty.network.send(PacketDistributor.PLAYER.with(() -> display), new BountyDisplays(player.getPersistentData().getString("bountydisplay1"), player.getPersistentData().getInt("bounty"), player.getPersistentData().getString("bountydisplay2"), player.getId(), 0));
+                PacketDistributor.sendToPlayer(display, new BountyDisplays(player.getPersistentData().getString("bountydisplay1"), player.getPersistentData().getInt("bounty"), player.getPersistentData().getString("bountydisplay2"), player.getId(), false));
             }
         }
     }
